569fc56d1c25e1bea38105999e507858fd298593
TBD <weinanshao@163.com>
2019-09-04 13:34:48 +0800

    init

---
+++ LocationPlugin/JWeixinNativeCodeHandler_getLocation+LocationPlugin.h
@@ -0,0 +1,24 @@
+//
+//  JWeixinNativeCodeHandler_getLocation+LocationPlugin.h
+//  LocationPlugin
+//
+//  Created by TBD on 2019/9/4.
+//
+//  Copyright (C) 2011-2019 ShenZhen iBOXCHAIN Information Technology Co.,Ltd. 
+//                             All rights reserved.
+//
+//      This  software  is  the  confidential  and  proprietary  information  of
+//  iBOXCHAIN  Company  of  China. ("Confidential Information"). You  shall  not
+//  disclose such Confidential Information and shall use it only in accordance
+//  with the terms of the contract agreement you entered into with iBOXCHAIN inc.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (JWeixinNativeCodeHandler_getLocation)
+
+@end
+
+NS_ASSUME_NONNULL_END
