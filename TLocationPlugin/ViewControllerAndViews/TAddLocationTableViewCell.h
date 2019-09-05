db23902c6580b57249d3146e2f549a28d93b7898
TBD <weinanshao@163.com>
2019-09-05 20:17:37 +0800

    修改注释

--- TLocationPlugin/ViewControllerAndViews/TAddLocationTableViewCell.h
+++ TLocationPlugin/ViewControllerAndViews/TAddLocationTableViewCell.h
@@ -3,14 +3,7 @@
 //  TLocationPlugin
 //
 //  Created by TBD on 2019/9/5.
-//
-//  Copyright (C) 2011-2019 ShenZhen iBOXCHAIN Information Technology Co.,Ltd. 
-//                             All rights reserved.
-//
-//      This  software  is  the  confidential  and  proprietary  information  of
-//  iBOXCHAIN  Company  of  China. ("Confidential Information"). You  shall  not
-//  disclose such Confidential Information and shall use it only in accordance
-//  with the terms of the contract agreement you entered into with iBOXCHAIN inc.
+//  Copyright © 2019 TBD. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
