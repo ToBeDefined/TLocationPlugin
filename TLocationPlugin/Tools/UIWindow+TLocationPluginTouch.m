9090054eef2179b3ce8b743a2cb62eb06fef07b2
TBD <weinanshao@163.com>
2019-09-06 23:46:11 +0800

    修改 touch 事件实现

--- TLocationPlugin/Tools/UIWindow+TLocationPluginTouch.m
+++ TLocationPlugin/Tools/UIWindow+TLocationPluginTouch.m
@@ -13,26 +13,26 @@
 
 @implementation UIWindow (TLocationPluginTouch)
 
-static NSInteger _t_window_sharkedTimes = 0;
+static NSInteger _t_windowTouchedTimes = 0;
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    ++_t_window_sharkedTimes;
-    if (_t_window_sharkedTimes <= 3) {
-        if (_t_window_sharkedTimes == 0) {
-            // 开始摇晃, 5秒内触摸四次, 5秒后清零
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                _t_window_sharkedTimes = 0;
-            });
-        }
-        return;
-    }
-    // 5秒内触摸四次
     if (TLocationNavigationController.isShowing) {
         return;
     }
-    _t_window_sharkedTimes = 0;
+    if (_t_windowTouchedTimes == 0) {
+        // 开始触摸, 5秒后清零
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            _t_windowTouchedTimes = 0;
+        });
+    }
+    ++_t_windowTouchedTimes;
+    if (_t_windowTouchedTimes < 5) {
+        return;
+    }
+    // 5秒内触摸5次
+    TLocationNavigationController.isShowing = YES;
+    _t_windowTouchedTimes = 0;
     AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
     UIViewController *rootVC = [UIApplication sharedApplication].keyWindow.rootViewController;
-    TLocationNavigationController.isShowing = YES;
     TSetLocationViewController *vc = [[TSetLocationViewController alloc] init];
     TLocationNavigationController *nav = [[TLocationNavigationController alloc] initWithRootViewController:vc];
     [rootVC presentViewController:nav animated:YES completion:^{
